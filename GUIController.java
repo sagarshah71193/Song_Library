90298fe96fb5191ddd2e450a7537996bcb22bc57
sagarshah71193 <sagarshah793.college@gmail.com>
2016-09-26 19:18:23 -0400

    Add files via upload

--- GUIController.java
+++ GUIController.java
@@ -1,5 +1,49 @@
 package GUI;
 
+import java.util.*;
+
+import javafx.fxml.FXML;
+
 public class GUIController {
+	
+	public class Song{
+		String name;
+		String artist;
+		String year;
+		String album;
+		
+		public Song(String name, String artist, String year, String album){
+			this.name = name;
+			this.artist = artist;
+			this.year = year;
+			this.album = album;
+		}
+		
+		public void setName(String s){
+			name = s;
+		}
+		public void setArtist(String s){
+			artist = s;
+		}
+		public void setYear (String s){
+			year = s;
+		}
+		public void setAlbum(String s){
+			album = s;
+		}
+		public String getName(){
+			return name;
+		}
+		public String getArtist(){
+			return artist;
+		}
+		public String getYear(){
+			return year;
+		}
+		public String getAlbum(){
+			return album;
+		}
+	}
+
 
 }
